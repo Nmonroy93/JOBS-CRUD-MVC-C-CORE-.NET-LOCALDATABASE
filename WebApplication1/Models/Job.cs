db32bb14d1ad90e448e0cab96f7c9bd90fc0812f
nestor monroy <nestormonroy93@yahoo.es>
2020-06-22 20:04:46 -0600

    se creo el proyecto con las clases y las vistas y controladores

---
+++ WebApplication1/Models/Job.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Job
+    {
+        [Display(Name = "Id")]
+        public int id { get; set; }
+
+
+
+        [RegularExpression(@"^[A-Za-z ]+$"), Required, StringLength(30, MinimumLength = 5)]
+        [Display(Name = "Job Title")]
+        public string titulo { get; set; }
+
+
+        [StringLength(80, MinimumLength = 5), Required]
+        [Display(Name = "Description")]
+        public string Descripcion { get; set; }
+
+
+        //[Range(typeof(DateTime), "1/1/1966", "1/10/2020")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [Display(Name = "Created At"), DataType(DataType.Date)]
+        public DateTime FechaF { get; set; }
+
+
+        //[Range(typeof(DateTime), "1/1/1966", "1/10/2020")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [Display(Name = "Expires At"), DataType(DataType.Date)]
+        public DateTime Fechai { get; set; }
+    }
+}
